b8b2a1eecc00365ebc0b3188035fd9da9a218123
electron2code <dev.israfil.code@gmail.com>
2025-12-04 07:01:18 +0000

    stack and heap memory in javaScript

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -37,3 +37,35 @@ const greet = function () {
 } // this is a function object
 
 console.log(typeof greet);
+
+
+
+// +++++++++++++++++++++ Memory for differents data types +++++++++++++++++++ 
+
+// Stack (primitive), Heap (Non-primitive)
+
+let score1 = 100;
+let score2 = score1;  // there copying value from score1, not score1's memory address;
+
+console.log(score2);
+score2 = 30;
+
+console.log(score1);
+
+const user1 = {
+    id: 10,
+    username: "israfil",
+    email: "israfil@google.com",
+}
+
+const user2 = user1 
+// there is copying user1 memory address in user2 if we change user2 this going to change user1 alson
+
+user2.email = "e2c@gmail.com";
+
+console.log(user1.email);
+
+/*
+    the value of a non-primitve is stored in heap memory and assigning the address
+    to a memory in stack changing value in the stack memory applys in heap memory address
+*/
